76f42b75bf3e91b035e6572a69ea4c9d768600a8
Purohit921 <147918295+Purohit921@users.noreply.github.com>
2024-08-13 11:16:29 +0530

    update

--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -12,28 +12,30 @@ import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function Testimonials() {
   return (
-    <Swiper
-      slidesPerView={3}
-      spaceBetween={30}
-      centeredSlides={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
-      pagination={{
-        clickable: true,
-      }}
-      navigation={true}
-      modules={[Autoplay, Pagination, Navigation]}
-      className="mySwiper"
-    >
-      <SwiperSlide className="testimonials">Testimonial 1</SwiperSlide>
-      <SwiperSlide className="testimonials">Testimonial 2</SwiperSlide>
-      <SwiperSlide className="testimonials">Testimonial 3</SwiperSlide>
-      <SwiperSlide className="testimonials">Testimonial 4</SwiperSlide>
-      <SwiperSlide className="testimonials">Testimonial 5</SwiperSlide>
-      <SwiperSlide className="testimonials">Testimonial 6</SwiperSlide>
-    </Swiper>
+    <div className="testimonials">
+      <Swiper
+        slidesPerView={3}
+        spaceBetween={30}
+        centeredSlides={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Autoplay, Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>Testimonial 1</SwiperSlide>
+        <SwiperSlide>Testimonial 2</SwiperSlide>
+        <SwiperSlide>Testimonial 3</SwiperSlide>
+        <SwiperSlide>Testimonial 4</SwiperSlide>
+        <SwiperSlide>Testimonial 5</SwiperSlide>
+        <SwiperSlide>Testimonial 6</SwiperSlide>
+      </Swiper>
+    </div>
   );
 }
 
--- src/pages/Category/Category.jsx
+++ src/pages/Category/Category.jsx
@@ -1,7 +1,14 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import { useParams } from "react-router-dom";
 import { StoreContext } from "../../context/StoreContext";
 import CategoryItem from "../../components/CategoryItem/CategoryItem";
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 function Category() {
   const { menu_list } = useContext(StoreContext);
@@ -9,9 +16,36 @@ function Category() {
   const category = menu_list.find((e) => e.menu_id === categoryId);
   console.log(category);
   return (
-    <div className="app-land">
-      <div className="App">
-        <CategoryItem categories={category.menu_name} />
+    <div className="header">
+      <div className="header-contents">
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          <SwiperSlide className="text-majhi">Banner 1</SwiperSlide>
+          <SwiperSlide className="text-majhi">Banner 2</SwiperSlide>
+          <SwiperSlide className="swiper-center">Banner 3</SwiperSlide>
+          <SwiperSlide className="swiper-center">Banner 4</SwiperSlide>
+          <SwiperSlide className="swiper-center">Banner 5</SwiperSlide>
+          <SwiperSlide className="swiper-center">Banner 6</SwiperSlide>
+          <SwiperSlide className="swiper-center">Banner 7</SwiperSlide>
+          <SwiperSlide className="swiper-center">Banner 8</SwiperSlide>
+        </Swiper>
+      </div>
+      <div className="app-land">
+        <div className="App">
+          <CategoryItem categories={category.menu_name} />
+        </div>
       </div>
     </div>
   );
