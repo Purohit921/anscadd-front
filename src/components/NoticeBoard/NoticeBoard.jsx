c7e344846c9e0fae36eef542755e30caf66a4add
Purohit921 <147918295+Purohit921@users.noreply.github.com>
2024-08-14 15:59:18 +0530

    noticeboard updated

--- src/components/NoticeBoard/NoticeBoard.jsx
+++ src/components/NoticeBoard/NoticeBoard.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import "./NoticeBoard.css";
+import { assets } from "../../assets/assets.js";
 
 function NoticeBoard() {
   return (
-    <div>
-      <div className="right-content">
+    <div className="noticeboard">
+      <div className="left-container">
         <h2>Notice Board</h2>
-        {/* <img src={assets.course_22} alt="" /> */}
+        <img src={assets.course_22} alt="" />
         <h3>Event</h3>
+      </div>
+      <div className="right-container">
+        <h2>Upcoming Events</h2>
+
       </div>
     </div>
   );
