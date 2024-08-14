54f5cf75e6b621c157c49ba7815223fd26ce7d06
Purohit921 <147918295+Purohit921@users.noreply.github.com>
2024-08-14 08:36:00 +0530

    category page updated

--- src/components/CategoryItem/CategoryItem.jsx
+++ src/components/CategoryItem/CategoryItem.jsx
@@ -2,25 +2,29 @@ import React, { useContext } from "react";
 import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 import CategoryCourse from "../CategoryCourse/CategoryCourse";
+import "./CategoryItem.css";
 
 function CategoryItem({ categories }) {
   const { food_list } = useContext(StoreContext);
   return (
-    <div>
-      {food_list.map((item, index) => {
-        if (categories === item.category) {
-          return (
-            <CategoryCourse
-              key={index}
-              id={item.id}
-              name={item.name}
-              description={item.description}
-              price={item.price}
-              image={item.image}
-            />
-          );
-        }
-      })}
+    <div className="food-display">
+      <h2>Courses for you</h2>
+      <div className="food-display-list">
+        {food_list.map((item, index) => {
+          if (categories === item.category) {
+            return (
+              <CategoryCourse
+                key={index}
+                id={item.id}
+                name={item.name}
+                description={item.description}
+                price={item.price}
+                image={item.image}
+              />
+            );
+          }
+        })}
+      </div>
     </div>
   );
 }
