fae2a3a7dcfae03398da983cf24fde37dc0bc8b9
Purohit921 <147918295+Purohit921@users.noreply.github.com>
2024-08-26 22:33:12 +0530

    fonts updated

--- src/components/Welcome/Welcome.jsx
+++ src/components/Welcome/Welcome.jsx
@@ -7,7 +7,7 @@ function Welcome() {
       <aside className="contact_aside">
         <div className="container categories_container">
           <h2>Welcome to ANS CADD</h2>
-          <div className="categories_right">
+          {/* <div className="categories_right">
             <article className="category">
               <span className="category_icon">
                 <i className="fa-solid fa-bitcoin-sign"></i>
@@ -41,7 +41,7 @@ function Welcome() {
                 Explore the wide variety of choices in designing.
               </p>
             </article>
-          </div>
+          </div> */}
         </div>
       </aside>
     </div>
